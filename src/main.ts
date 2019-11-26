8f51d11d1028d34a37899db5362d16463babe1d5
kaczor6418 <andrzej6418@gmail.com>
2019-11-26 20:46:05 +0100

    feat(DESC): add sort descending option

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import {SortDirection} from "./Enums";
 
 function insertionSort<T>(elementsList: T[], direction: SortDirection = SortDirection.ASC): void {
+    const condition: <T>(comparedElement: T, selectedElement: T) => boolean = direction === SortDirection.ASC ? ascendingCondition : descendingCondition;
     for (let selectedElementIndex: number = 1; selectedElementIndex < elementsList.length; selectedElementIndex++) {
         const key: T = elementsList[selectedElementIndex];
         let comparedElementIndex: number = selectedElementIndex - 1;
-        while (comparedElementIndex >= 0 && elementsList[comparedElementIndex] > key) {
+        while (comparedElementIndex >= 0 && condition(elementsList[comparedElementIndex], key)) {
             elementsList[comparedElementIndex + 1] = elementsList[comparedElementIndex];
             comparedElementIndex--;
         }
@@ -12,6 +13,12 @@ function insertionSort<T>(elementsList: T[], direction: SortDirection = SortDire
     }
 }
 
-const examplerray = [31, 41, 59, 26, 41, 58];
-insertionSort(examplerray);
-console.log(examplerray);
+const ascendingCondition: <T>(comparedElement: T, selectedElement: T) => boolean = <T>(comparedElement: T, selectedElement: T) => comparedElement > selectedElement;
+const descendingCondition: <T>(comparedElement: T, selectedElement: T) => boolean = <T>(comparedElement: T, selectedElement: T) => comparedElement < selectedElement;
+
+const exampleArray1 = [31, 41, 59, 26, 41, 58];
+const exampleArray2 = [31, 41, 59, 26, 41, 58];
+insertionSort(exampleArray1, SortDirection.ASC);
+insertionSort(exampleArray2, SortDirection.DESC);
+console.log(exampleArray1);
+console.log(exampleArray2);
