8f65a18ad24493cf2147e72029fef872d5786c17
kaczor6418 <andrzej6418@gmail.com>
2019-12-14 23:33:47 +0100

    ref(generic): replace not needed generic type with number type

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import {SortDirection} from "./Enums";
 
-function insertionSort<T>(elementsList: T[], direction: SortDirection = SortDirection.ASC): void {
-    const condition: <T>(comparedElement: T, selectedElement: T) => boolean = direction === SortDirection.ASC ? ascendingCondition : descendingCondition;
+function insertionSort(elementsList: number[], direction: SortDirection = SortDirection.ASC): void {
+    const condition: (comparedElement: number, selectedElement: number) => boolean = direction === SortDirection.ASC ? ascendingCondition : descendingCondition;
     for (let selectedElementIndex: number = 1; selectedElementIndex < elementsList.length; selectedElementIndex++) {
-        const key: T = elementsList[selectedElementIndex];
+        const key: number = elementsList[selectedElementIndex];
         let comparedElementIndex: number = selectedElementIndex - 1;
         while (comparedElementIndex >= 0 && condition(elementsList[comparedElementIndex], key)) {
             elementsList[comparedElementIndex + 1] = elementsList[comparedElementIndex];
@@ -13,5 +13,5 @@ function insertionSort<T>(elementsList: T[], direction: SortDirection = SortDire
     }
 }
 
-const ascendingCondition: <T>(comparedElement: T, selectedElement: T) => boolean = <T>(comparedElement: T, selectedElement: T) => comparedElement > selectedElement;
-const descendingCondition: <T>(comparedElement: T, selectedElement: T) => boolean = <T>(comparedElement: T, selectedElement: T) => comparedElement < selectedElement;
+const ascendingCondition: (comparedElement: number, selectedElement: number) => boolean = (comparedElement: number, selectedElement: number) => comparedElement > selectedElement;
+const descendingCondition: (comparedElement: number, selectedElement: number) => boolean = (comparedElement: number, selectedElement: number) => comparedElement < selectedElement;
